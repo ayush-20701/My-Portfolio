f91ef6d5eddb11c34a81dfe8006039b652a9cd8c
Ayush Kumar <ayushkumar20701@gmail.com>
2025-04-25 16:43:38 +0530

    navbar done with dark mode

---
+++ Frontend/Data/NavbarData.js
@@ -0,0 +1,33 @@
+const NavbarData = [
+    {
+        key: "github",
+        text: "Github",
+        class: ""
+    },
+    {
+        key: "linkedin",
+        text: "LinkedIn",
+        class: ""
+    },
+    {
+        key: "leetcode",
+        text: "Leetcode",
+        class: ""
+    },
+    {
+        key: "resume",
+        text: "Resume",
+        class: ""
+    },
+    {
+        key: "projects",
+        text: "Projects",
+        class: ""
+    },
+    {
+        key: "contact",
+        text: "Contact Me",
+        class: ""
+    }
+]
+export default NavbarData;
