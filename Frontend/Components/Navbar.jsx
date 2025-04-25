df004f4cf5bd99b601536383dcff2ed0b7fd877b
Ayush Kumar <ayushkumar20701@gmail.com>
2025-04-25 13:47:58 +0530

    initial commit

---
+++ Frontend/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        Navbar
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../Components/Navbar'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
